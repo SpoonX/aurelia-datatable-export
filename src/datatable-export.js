1f97e4decdb0a2ac65e8cfb3dde78efeca615c95
jeremyvergnas <jeremy.vergnas@viacesi.fr>
2016-10-05 16:33:26 +0200

    refactor(project): trigger event through datatable

--- src/datatable-export.js
+++ src/datatable-export.js
@@ -10,28 +10,30 @@ export class DatatableExport {
   @bindable criteria  = {};
 
   doExport() {
-    if (this.datatable) {
-      return this.datatable.gatherData(this.criteria).then(result => {
-        try {
-          let csv  = json2csv({data: result, fields: this.columns});
-          let blob = new Blob([csv]);
-
-          if (window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveBlob(blob, "export.csv");
-          }
-          else {
-            let a = window.document.createElement("a");
-
-            a.href     = window.URL.createObjectURL(blob, {type: "text/plain"});
-            a.download = "export.csv";
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-          }
-        } catch (err) {
-          throw Error(err);
-        }
-      });
+    if (!this.datatable) {
+      return;
     }
+
+    return this.datatable.gatherData(this.criteria).then(result => {
+      try {
+        let csv  = json2csv({data: result, fields: this.columns});
+        let blob = new Blob([csv]);
+
+        if (window.navigator.msSaveOrOpenBlob) {
+          window.navigator.msSaveBlob(blob, "export.csv");
+        }
+        else {
+          let a = window.document.createElement("a");
+
+          a.href     = window.URL.createObjectURL(blob, {type: "text/plain"});
+          a.download = "export.csv";
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+        }
+      } catch (err) {
+        this.datatable.triggerEvent('exception', {on: 'export', error: err});
+      }
+    });
   }
 }
