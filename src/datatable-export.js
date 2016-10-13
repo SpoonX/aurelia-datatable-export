45033439b1e74f2b564aa8d6e2fad111a358c274
jeremyvergnas <jeremy.vergnas@viacesi.fr>
2016-10-13 14:05:45 +0200

    feat(project): Add multiple export formats support

--- src/datatable-export.js
+++ src/datatable-export.js
@@ -1,6 +1,7 @@
-import {bindable, customElement} from 'aurelia-templating';
-import {resolvedView} from 'aurelia-view-manager';
-import json2csv from 'json2csv';
+import {bindable, customElement}  from 'aurelia-templating';
+import {resolvedView}             from 'aurelia-view-manager';
+import json2csv                   from 'json2csv';
+import js2xmlparser               from 'js2xmlparser';
 
 @customElement('datatable-export')
 @resolvedView('aurelia-datatable-export', 'datatable-export')
@@ -8,31 +9,74 @@ export class DatatableExport {
   @bindable columns   = [];
   @bindable datatable = null
   @bindable criteria  = {};
+  @bindable format    = 'csv';
+  @bindable filename  = 'export'
 
   doExport() {
-    if (!this.datatable) {
+    if (!this.datatable || !this.format) {
       return;
     }
 
-    return this.datatable.gatherData(this.criteria).then(result => {
-      try {
-        let csv  = json2csv({data: result, fields: this.columns});
-        let blob = new Blob([csv]);
+    let rawColumns = (typeof this.columns === 'string' ? this.getColumns().split(',') : this.columns);
 
-        if (window.navigator.msSaveOrOpenBlob) {
-          return window.navigator.msSaveBlob(blob, 'export.csv');
-        }
-
-        let a = window.document.createElement('a');
-
-        a.href     = window.URL.createObjectURL(blob, {type: 'text/plain'});
-        a.download = 'export.csv';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-      } catch (err) {
-        this.datatable.triggerEvent('exception', {on: 'export', error: err});
-      }
+    this.datatable.gatherData(this.criteria).then(result => {
+      this[this.format](result, rawColumns);
     });
+  }
+
+  getColumns() {
+    let cleanedColumns = this.columns;
+
+    // Remove aliases and value converters
+    cleanedColumns = cleanedColumns.replace(/\sas\s.*[\,]/gi, ',');
+    cleanedColumns = cleanedColumns.replace(/\s\|\s.*[\,]/gi, ',');
+
+    if (cleanedColumns.indexOf(' as ') > -1) {
+      cleanedColumns = cleanedColumns.substring(0, cleanedColumns.indexOf(' as '));
+    }
+    if (cleanedColumns.indexOf(' | ') > -1) {
+      cleanedColumns = cleanedColumns.substring(0, cleanedColumns.indexOf(' | '));
+    }
+
+    cleanedColumns = cleanedColumns.replace(/\s/g, '');
+
+    return cleanedColumns;
+  }
+
+  download(data) {
+    let blob = new Blob([data]);
+
+    if (window.navigator.msSaveOrOpenBlob) {
+      return window.navigator.msSaveBlob(blob, this.filename + '.' + this.format);
+    }
+
+    let a = window.document.createElement('a');
+
+    a.href     = window.URL.createObjectURL(blob, {type: 'text/plain'});
+    a.download = this.filename + '.' + this.format;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+  csv(data, columns) {
+    try {
+      let csv = json2csv({data: data, fields: columns});
+
+      this.download(csv);
+    } catch (err) {
+      this.datatable.triggerEvent('exception', {on: 'export', error: err});
+    }
+  }
+
+  xml(data) {
+    try {
+      let xml = js2xmlparser.parse(this.datatable.resource, data);
+
+      this.download(xml);
+    } catch (err) {
+      this.datatable.triggerEvent('exception', {on: 'export', error: err});
+    }
+
   }
 }
